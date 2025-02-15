cd0030225789bac745be9e7d6888ae4689bc39bd
Michael Berger <vo1kod4v@gmail.com>
2025-02-15 21:45:17 +0200

    Add support to run a grid of experiments fix in summarize_profile added benchmark cases permutation func (not needed really)

--- Benchmarking.py
+++ Benchmarking.py
@@ -8,7 +8,7 @@ import torch
 from ArticleReader.LatexToSpeech import Chunker
 import pandas as pd
 from speechbrain.inference import Tacotron2, HIFIGAN
-
+import json
 
 class MemoryMonitor:
     """
@@ -56,18 +56,21 @@ class MemoryMonitor:
 class Bench:
 
     def summarize_profile(self, model_profiler):
-        data = pd.DataFrame(model_profiler.memory_log)
-        data['time'] = pd.to_datetime(data['time'], unit='s')
-        if len(data.time)>0:
+
+        if len(model_profiler.memory_log)>1:
+            data = pd.DataFrame(model_profiler.memory_log)
+            data['time'] = pd.to_datetime(data['time'], unit='s')
             dur = (data.time.max() - data.time.min()).total_seconds() 
+            memuse = data['memory'].max()
             #dur = str(dur.microseconds/1e6)
         else:
             dur=0
+            memuse=None
             
         res = {
             "model_id": model_profiler.model_id ,  #(name)
             "stage": model_profiler.stage,
-            "max_memory_use": data['memory'].max(),
+            "max_memory_use": memuse,
             "run_time_sec": dur,
             "memory_log": model_profiler.memory_log,
             "exceptions": model_profiler.exception,
@@ -86,7 +89,7 @@ class Bench:
         """
         tstp = datetime.now().strftime(r"%y.%m.%d-%H.%M.%S")
         case_file = "benchmark/" + tstp
-        
+
         self.chunker = Chunker(max_len=case["chunk_length"])
         self.chunker.split_text_into_chunks(processed)
         
@@ -132,21 +135,193 @@ class Bench:
         perc_sile = 1- sum(durations)/(max(durations)*len(durations))
 
         
-        parameters = {
+        result = {
             "time": tstp,
             "experiment_id": tstp,
             "chunk_durations": durations_sec,
             "avg_percent_silence": perc_sile
         }
-        case.update(parameters)
+        result.update(case)
         
         tts_stage = self.summarize_profile(self.tts_profiler)
-        tts_stage.update(case)
+        tts_stage.update(result)
         
         voc_stage = self.summarize_profile(self.vocoder_profiler)
-        voc_stage.update(case)
+        voc_stage.update(result)
 
         return [tts_stage, voc_stage]
+
+
+
+    def run_experiments(self, processed_text, grid):
+        """
+        grid = {"chunk_length": range(50, 500, 50),
+                "batch_size": (1, 2, 3, 5, 10, 20, 30, 50, 70, 100, 200),
+                "tts_model": ["tts-tacotron2-ljspeech"],
+                "vocoder_model": ["tts-hifigan-ljspeech"],
+                "device": ["CPU"], 
+            }
+        grid
+        """        
+        self.provider = "speechbrain"
+        
+        self.case_objects = {}
+        self.case = {}
+
+        for d in grid["device"]:
+            self.init_device(d)
+
+            for tts_model_name in grid["tts_model"]:                
+                self.init_tts_model(tts_model_name)
+
+                for voc_model_name in grid["vocoder_model"]:     
+                    self.init_voc_model(voc_model_name)
+
+                    for chunk_length in grid["chunk_length"]:
+                        self.init_chunker(processed_text, chunk_length)
+
+                        for batch_size in grid["batch_size"]:
+                            self.init_batch(batch_size)
+
+
+                            print(f"running experiment:\n {json.dumps(self.case)}")
+                            wat = json.dumps(self.case_objects["batch_size"])
+                            print(f"test data:\n {wat}")
+                            experiment_run = self.run_case()
+                            with open("benchmark/" + experiment_run[0]["experiment_id"] + ".json", "w+") as f:
+                                json.dump(experiment_run,f)
+
+    def init_batch(self, batch_size):
+        fr = 25 # beginning from chunk
+        batch = self.chunker.get_test_batch(batch_size, fr)        
+                          
+        self.case_objects["batch_size"] = batch
+        self.case["batch_size"] = batch_size
+
+    def init_chunker(self, processed_text, chunk_length):
+        self.chunker = Chunker(max_len=chunk_length)
+        self.chunker.split_text_into_chunks(processed_text)                        
+        self.case_objects["chunk_length"] = self.chunker
+        self.case["chunk_length"] = chunk_length
+
+    def init_voc_model(self, voc_model_name):
+        vocoder_model = self.vocoder = HIFIGAN.from_hparams(
+                        source=f"{self.provider}/{voc_model_name}",
+                        savedir=f"checkpoints/{voc_model_name}",
+                        run_opts={"device":self.case_objects["device"]} 
+                        )
+        vocoder_model.id = voc_model_name
+
+        self.vocoder_profiler = MemoryMonitor(stage="vocoder", model_id=vocoder_model.id)
+        vocoder_model.decode_batch = self.vocoder_profiler.attach_to(vocoder_model.decode_batch)
+                    
+        self.case_objects["vocoder_model"] = vocoder_model
+        self.case["vocoder_model"] = voc_model_name
+
+    def init_device(self, d):
+        dev = "cuda" if d =="GPU" else "cpu"            
+        self.case_objects["device"] = dev
+        self.case["device"] = d
+        return dev
+
+    def init_tts_model(self, tts_model_name):
+
+        tts_model = Tacotron2.from_hparams(
+                    source=f"{self.provider}/{tts_model_name}",
+                    savedir=f"checkpoints/{tts_model_name}",
+                    overrides={"max_decoder_steps": 2000},
+                    run_opts={"device":self.case_objects["device"]} 
+                    )
+        tts_model.id = tts_model_name
+        self.tts_profiler = MemoryMonitor(stage="tts", model_id=tts_model.id)
+        tts_model.encode_batch = self.tts_profiler.attach_to(tts_model.encode_batch)
+
+        self.case_objects["tts_model"] = tts_model
+        self.case["tts_model"] = tts_model_name
+
+    def run_case(self):
+
+        sampling_freq = 22050.0
+        tstp = datetime.now().strftime(r"%y.%m.%d-%H.%M.%S")
+        case_file = "benchmark/" + tstp
+
+        device = self.case_objects["device"]
+        tts_model = self.case_objects["tts_model"]
+        vocoder_model = self.case_objects["vocoder_model"]
+        chunk_length = self.case_objects["chunk_length"]
+        batch= self.case_objects["batch_size"]
+
+
+        self.narrator = Narrator(tts_model, vocoder_model) 
+        waveforms, durations = self.narrator.text_to_speech_batched(batch)
+        waveform = torch.cat(waveforms, dim=1)
+        self.narrator.save_audio(case_file + ".wav", waveform)
+
+        self.chunker.save_chunks_as_text(case_file + ".md", batch)
+
+        # create a report
+        durations_sec = (durations / sampling_freq).tolist()
+        perc_sile = 1- sum(durations)/(max(durations)*len(durations))
+        
+        result = {
+            "time": tstp,
+            "experiment_id": tstp,
+            "chunk_durations": durations_sec,
+            "avg_percent_silence": perc_sile
+        }
+        result.update(self.case)
+        
+        tts_stage = self.summarize_profile(self.tts_profiler)
+        tts_stage.update(result)
+        
+        voc_stage = self.summarize_profile(self.vocoder_profiler)
+        voc_stage.update(result)
+
+        return [tts_stage, voc_stage]
+
+
+    def test_permutations(self):
+
+        grid = {"A": [1,2,3,4,5,6],
+             "B": "a b c d e f g h i j".split(' '),
+             "C": ["U", "V"],
+             "D": ["J","K"],
+             "E": ["P"], 
+               }
+        res = self.permutations(grid)
+        
+        import json
+        with open("check.json", 'w+') as f: 
+            json.dump(res, f, indent=4)
+
+
+        import pandas as pd 
+        df = pd.read_json("check.json")
+        print(df)
+
+        print(len(df.drop_duplicates())) 
+
+    def permutations(self, grid):
+
+        keys = list(grid.keys())
+        n = len(keys)
+
+        layers = [[{keys[l]:v} for v in grid[keys[l]]]  for l in range(n)]
+
+        def combine(prev, this):
+            print(prev)
+            print(this)
+            tmp = [ t.copy() for t in this]
+            [th.update(prev) for th in tmp]
+            return tmp
+
+        res = layers[0]
+        for i in range(1,n):
+            l1 = res
+            l2 = layers[i]
+            res = [combine(l, l2) for l in l1]
+            res = sum(res,[])
+        return res
 
 
 
